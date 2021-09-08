a6045740242dae3d58a113b6e801b8b4a8ee86b8
Casey Kriutzfield <casey.kriutzfield@live.com>
2021-09-08 18:25:19 -0400

    Updated the quote stuff.

--- api/HttpQuote/index.js
+++ api/HttpQuote/index.js
@@ -11,5 +11,21 @@ module.exports = async function (context, req) {
         //body: responseMessage
     //};
 
-    context.res.json({"author":"Pranshu Midha", "quote":"Happiness should be a function without any parameters."});
+    const data = [
+        {"author":"Pranshu Midha", "quote":"Happiness should be a function without any parameters."},
+        {"author":"Winston Churchill", "quote":"If you're going through hell, keep going."},
+        {"author":"Kirk Douglas", "quote":"In order to achieve anything you must be brave enough to fail."},
+        {"author":"Alan Kay", "quote":"The most disastrous thing that you can ever learn is your first programming language."},
+        {"author":"Raghu Venkatesh", "quote":"If you can write \"hello world\" you can change the world."},
+        {"author":"Unknown", "quote":"Be brave enough to suck at something new."},
+        {"author":"Scott Adams", "quote":"Engineers like to solve problems, if there are no problems available, they will create their own."}
+    ]
+
+    quoteLength = data.length;
+    context.res.json(data[getRandomInt(quoteLength)]);
+}
+
+// This will give us a random number between 0 and the max.
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
